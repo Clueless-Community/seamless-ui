26045a2f243d1d4910aa525a4c54072641cddb15
Arijit Ghosh <92109154+Rancho2002@users.noreply.github.com>
2022-10-22 01:54:43 +0530

    Add files via upload

---
+++ Create a widget list-item-6/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      // ...
+      colors:{
+      main : "#1956cb",
+    },
+  },
+  plugins: [],
+}
+}
