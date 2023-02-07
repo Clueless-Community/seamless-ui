6db0cbc46584bf5c7be191f8002059d13ca17e0c
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2023-02-08 01:12:47 +0530

    testing..

---
+++ components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div><h1 className="text-center font-extrabold md:text-5xl mt-8">SeamLess UI</h1></div>
+  )
+}
+
+export default Hero
