7c1c96a2720a2cc475af3643c2d3d6e9309890b2
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2023-04-07 02:51:41 +0530

    SEO Improved and pages setup done

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/docs/components/buttons":["static\u002Fchunks\u002Fpages\u002Fdocs\u002Fcomponents\u002Fbuttons.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdocs\u002Fcomponents\u002Fbuttons"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
