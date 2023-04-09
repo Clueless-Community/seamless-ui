57c35682927cf9efa6962ca0c59acc477d161682
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-09 21:55:21 +0530

    breadcrumb added but finishing needed

---
+++ database/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,335 @@
+import React from "react";
+
+export const NormalBreadcrumb = () => {
+  return (
+    <div>
+      <div className="my-4">
+        <div className="inline-flex flex-wrap">
+          <a
+            className="breadcrumb-link text-slate-500 font-semibold focus:text-blue-600 hover:text-blue-600 px-3 py-2 transition-colors duration-300 ease-in-out"
+            href="#"
+          >
+            <span>Dashboard</span>
+          </a>
+        </div>
+      </div>
+
+      <div className="my-4">
+        <div className="inline-flex items-center flex-wrap">
+          <a
+            className="breadcrumb-link text-slate-500 font-semibold focus:text-blue-600 hover:text-blue-600 px-3 py-2 transition-colors duration-300 ease-in-out"
+            href="#"
+          >
+            <span>Dashboard</span>
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 mx-3 text-slate-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+          <a
+            className="breadcrumb-link text-slate-500 font-semibold focus:text-blue-600 hover:text-blue-600 px-3 py-2 transition-colors duration-300 ease-in-out"
+            href="#"
+          >
+            <span>Components</span>
+          </a>
+        </div>
+      </div>
+
+      <div className="my-4">
+        <div className="inline-flex items-center flex-wrap">
+          <a
+            className="breadcrumb-link text-slate-500 font-semibold focus:text-blue-600 hover:text-blue-600 px-3 py-2 transition-colors duration-300 ease-in-out"
+            href="#"
+          >
+            <span>Dashboard</span>
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 mx-3 text-slate-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+          <a
+            className="breadcrumb-link text-slate-500 font-semibold focus:text-blue-600  hover:text-blue-600 px-3 py-2 transition-colors duration-300 ease-in-out"
+            href="#"
+          >
+            <span>Components</span>
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 mx-3 text-slate-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+          <a
+            className="breadcrumb-link text-slate-500 font-semibold px-3 py-2 transition-colors duration-300 ease-in-out hover:text-blue-600 focus:text-blue-600"
+            href="#"
+          >
+            <span>Breadcrumbs</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const BoxedBreadcrumb = () => {
+  return (
+    <div>
+      <div className="my-4 mx-4">
+        <div className="inline-flex flex-wrap">
+          <a
+            className="text-slate-500 font-semibold bg-gray-200 px-5 py-2 rounded-l-lg rounded-r-lg hover:text-white hover:ease-in-out hover:bg-blue-600 focus:bg-blue-600 transition focus:ease-in-out focus:text-white"
+            href="#"
+          >
+            <span>Dashboard</span>
+          </a>
+        </div>
+      </div>
+
+      <div className="my-4 mx-4">
+        <div className="inline-flex items-center flex-wrap">
+          <a
+            className="text-slate-500 font-semibold bg-gray-200 px-5 py-2 rounded-l-lg rounded-r-lg hover:text-white hover:ease-in-out hover:bg-blue-600 focus:bg-blue-600 transition focus:ease-in-out focus:text-white"
+            href=""
+          >
+            <span>Dashboard</span>
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 mx-3 text-slate-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+          <a
+            className="text-slate-500 font-semibold bg-gray-200 px-5 py-2 rounded-l-lg rounded-r-lg hover:text-white hover:ease-in-out hover:bg-blue-600 focus:bg-blue-600 transition focus:ease-in-out focus:text-white"
+            href=""
+          >
+            <span>Components</span>
+          </a>
+        </div>
+      </div>
+
+      <div className="my-4 mx-4">
+        <div className="inline-flex items-center flex-wrap gap-y-4">
+          <a
+            className="text-slate-500 font-semibold bg-gray-200 px-5 py-2 rounded-l-lg rounded-r-lg hover:text-white hover:ease-in-out hover:bg-blue-600 focus:bg-blue-600 transition focus:ease-in-out focus:text-white"
+            href="#"
+          >
+            <span>Dashboard</span>
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 mx-3 text-slate-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+          <a
+            className="text-slate-500 font-semibold bg-gray-200 px-5 py-2 rounded-l-lg rounded-r-lg hover:text-white hover:ease-in-out hover:bg-blue-600 focus:bg-blue-600 transition focus:ease-in-out focus:text-white"
+            href="#"
+          >
+            <span>Components</span>
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 mx-3 text-slate-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+          <a
+            className="text-slate-500 font-semibold bg-gray-200 px-5 py-2 rounded-l-lg rounded-r-lg hover:text-white hover:ease-in-out hover:bg-blue-600 focus:bg-blue-600 transition focus:ease-in-out focus:text-white"
+            href="#"
+          >
+            <span>Breadcumbs</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const IconBreadcrumb = () => {
+  return (
+    <div>
+      <div className="flex items-center overflow-x-auto whitespace-nowrap py-4">
+        <a href="#" className="text-gray-600 focus:text-blue-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+          </svg>
+        </a>
+
+        <span className="mx-5 text-gray-500 rtl:-scale-x-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+
+        <a
+          href="#"
+          className="-px-2 flex items-center text-gray-600  focus:text-blue-600 hover:underline"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-2 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+            />
+          </svg>
+
+          <span className="mx-2 font-semibold">Account</span>
+        </a>
+
+        <span className="mx-5 text-gray-500 rtl:-scale-x-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+
+        <a
+          href="#"
+          className="-px-2 flex items-center text-gray-600  focus:text-blue-600 hover:underline"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-2 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+            />
+          </svg>
+
+          <span className="mx-2 font-semibold">Profile</span>
+        </a>
+
+        <span className="mx-5 text-gray-500 rtl:-scale-x-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+
+        <a
+          href="#"
+          className="-px-2 flex items-center text-gray-600  focus:text-blue-600 hover:underline"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-2 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+
+          <span className="mx-2 font-semibold">Settings</span>
+        </a>
+      </div>
+    </div>
+  );
+};
