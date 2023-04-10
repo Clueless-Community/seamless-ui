977f0265f8af78ab167c6cda0743495d2b7764bf
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-10 15:44:57 +0530

    feature card 8 added

--- database/featureCards/featureCards.tsx
+++ database/featureCards/featureCards.tsx
@@ -604,7 +604,7 @@ export const FeatureCard6Dark = () => {
   );
 };
 
-// ------------Feature Card 5 Light--------------
+// ------------Feature Card 7 Light--------------
 export const FeatureCard7Light = () => {
   return (
     <div>
@@ -633,7 +633,7 @@ export const FeatureCard7Light = () => {
   );
 };
 
-// ------------Feature Card 5 Blue--------------
+// ------------Feature Card 7 Blue--------------
 export const FeatureCard7Blue = () => {
   return (
     <div>
@@ -662,7 +662,7 @@ export const FeatureCard7Blue = () => {
   );
 };
 
-// ------------Feature Card 5 Dark--------------
+// ------------Feature Card 7 Dark--------------
 export const FeatureCard7Dark = () => {
   return (
     <div>
@@ -687,6 +687,97 @@ export const FeatureCard7Dark = () => {
           Slate helps you see how many more days you need to work to reach.
           Slate.
         </p>
+      </div>
+    </div>
+  );
+};
+
+// ------------Feature Card 8 Light--------------
+export const FeatureCard8Light = () => {
+  return (
+    <div>
+      <div className="flex w-fit cursor-default flex-col items-center justify-center rounded-2xl bg-white px-7 py-6 shadow-slate-900 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-md">
+        <svg
+          width="54"
+          height="60"
+          viewBox="0 0 54 60"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M53.2083 14.5901C53.2083 40.3699 49.4868 52.9375 27.4255 59.2367C27.2868 59.2768 27.1448 59.2985 27.003 59.2985C26.8612 59.2985 26.7195 59.2768 26.5807 59.2367C4.51638 52.9313 0.791668 40.4929 0.791668 14.9876C0.791668 14.2969 1.25113 13.6927 1.91404 13.5046C13.7633 10.1592 22.7882 4.28891 25.964 1.15624C26.2569 0.869491 26.67 0.647359 27.0585 0.712109C27.4655 0.715192 27.8571 0.881463 28.1438 1.1713C31.0144 4.08196 43.1321 10.1129 52.1539 13.1222C52.7829 13.3381 53.2083 13.9272 53.2083 14.5901Z"
+            fill="#121225"
+          />
+        </svg>
+        <h1 className="mt-4 text-2xl font-bold leading-8 tracking-wide text-black">
+          Security
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+// ------------Feature Card 8 Blue--------------
+export const FeatureCard8Blue = () => {
+  return (
+    <div>
+      <div className="flex w-fit cursor-default flex-col items-center justify-center rounded-2xl bg-white px-7 py-6 shadow-slate-900 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-md">
+        <svg
+          width="54"
+          height="60"
+          viewBox="0 0 54 60"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <svg
+            width="53"
+            height="59"
+            viewBox="0 0 53 59"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M52.4167 13.8893C52.4167 39.6691 48.6951 52.2367 26.6338 58.5359C26.4951 58.576 26.3532 58.5977 26.2113 58.5977C26.0695 58.5977 25.9278 58.576 25.789 58.5359C3.72471 52.2305 0 39.7921 0 14.2868C0 13.5961 0.459459 12.9919 1.12238 12.8038C12.9716 9.45841 21.9965 3.5881 25.1723 0.455436C25.4653 0.168686 25.8784 -0.0534469 26.2669 0.0113031C26.6739 0.0143864 27.0654 0.180658 27.3522 0.470491C30.2227 3.38116 42.3404 9.41209 51.3622 12.4214C51.9912 12.6373 52.4167 13.2264 52.4167 13.8893Z"
+              fill="#1855CB"
+            />
+          </svg>
+        </svg>
+        <h1 className="mt-4 text-2xl font-bold leading-8 tracking-wide text-blue-600">
+          Security
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+// ------------Feature Card 8 Dark--------------
+export const FeatureCard8Dark = () => {
+  return (
+    <div>
+      <div className="flex w-fit cursor-default flex-col items-center justify-center rounded-2xl bg-gray-900 px-7 py-6 shadow-slate-900 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-md">
+        <svg
+          width="54"
+          height="60"
+          viewBox="0 0 54 60"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <svg
+            width="53"
+            height="59"
+            viewBox="0 0 53 59"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M52.4167 13.8893C52.4167 39.6691 48.6951 52.2367 26.6338 58.5359C26.4951 58.576 26.3532 58.5977 26.2113 58.5977C26.0695 58.5977 25.9278 58.576 25.789 58.5359C3.72471 52.2305 0 39.7921 0 14.2868C0 13.5961 0.459459 12.9919 1.12238 12.8038C12.9716 9.45841 21.9965 3.5881 25.1723 0.455436C25.4653 0.168686 25.8784 -0.0534469 26.2669 0.0113031C26.6739 0.0143864 27.0654 0.180658 27.3522 0.470491C30.2227 3.38116 42.3404 9.41209 51.3622 12.4214C51.9912 12.6373 52.4167 13.2264 52.4167 13.8893Z"
+              fill="white"
+            />
+          </svg>
+        </svg>
+        <h1 className="mt-4 text-2xl font-bold leading-8 tracking-wide text-white">
+          Security
+        </h1>
       </div>
     </div>
   );
