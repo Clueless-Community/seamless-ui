edd987b093276a356e52d901f79ddb83ed7b6abb
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-10 16:30:47 +0530

    feature card 9 added

--- database/featureCards/featureCards.tsx
+++ database/featureCards/featureCards.tsx
@@ -783,7 +783,7 @@ export const FeatureCard8Dark = () => {
   );
 };
 
-// ------------Feature Card 8 Light--------------
+// ------------Feature Card 9 Light--------------
 export const FeatureCard9Light = () => {
   return (
     <div>
@@ -804,7 +804,7 @@ export const FeatureCard9Light = () => {
   );
 };
 
-// ------------Feature Card 8 Blue--------------
+// ------------Feature Card 9 Blue--------------
 export const FeatureCard9Blue = () => {
   return (
     <div>
@@ -825,7 +825,7 @@ export const FeatureCard9Blue = () => {
   );
 };
 
-// ------------Feature Card 8 Dark--------------
+// ------------Feature Card 9 Dark--------------
 export const FeatureCard9Dark = () => {
   return (
     <div>
@@ -841,6 +841,81 @@ export const FeatureCard9Dark = () => {
           src="https://i.ibb.co/BKCPqvt/Web3-Report-Featured-image-2-5.png"
           alt="Web3-Report-Featured-image-2-5"
         />
+      </div>
+    </div>
+  );
+};
+
+// ------------Feature Card 9 Light--------------
+export const FeatureCard10Light = () => {
+  return (
+    <div>
+      <div className="w-fit flex cursor-default rounded-2xl  px-6 py-5 transition-all duration-500 ease-in-out  hover:shadow-slate-900/20 items-center justify-s bg-white gap-4">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M28.9517 13.3877L18.6125 3.04848C17.2825 1.71848 15.4458 0.958374 13.5775 0.958374H6.5C3.44416 0.958374 0.958328 3.44421 0.958328 6.50004V13.5775C0.958328 15.4775 1.70251 17.2666 3.04834 18.6124L13.3875 28.9516C14.7333 30.2974 16.5225 31.0417 18.4225 31.0417C20.3383 31.0417 22.1275 30.2974 23.4733 28.9516L28.9517 23.4733C31.7383 20.6867 31.7383 16.1744 28.9517 13.3877ZM8.875 11.25C7.56083 11.25 6.5 10.1892 6.5 8.87504C6.5 7.56087 7.56083 6.50004 8.875 6.50004C10.1892 6.50004 11.25 7.56087 11.25 8.87504C11.25 10.1892 10.1892 11.25 8.875 11.25Z"
+            fill="#121225"
+          />
+        </svg>
+        <p className="text-lg font-normal  text-gray-600 text-center">
+          Ultimate Discount{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+// ------------Feature Card 9 Blue--------------
+export const FeatureCard10Blue = () => {
+  return (
+    <div>
+      <div className="flex w-fit cursor-default rounded-2xl  items-center bg-white gap-4 px-6 py-5 ">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="#2D70F3"
+            d="M28.9517 13.3877L18.6125 3.04848C17.2825 1.71848 15.4458 0.958374 13.5775 0.958374H6.5C3.44416 0.958374 0.958328 3.44421 0.958328 6.50004V13.5775C0.958328 15.4775 1.70251 17.2666 3.04834 18.6124L13.3875 28.9516C14.7333 30.2974 16.5225 31.0417 18.4225 31.0417C20.3383 31.0417 22.1275 30.2974 23.4733 28.9516L28.9517 23.4733C31.7383 20.6867 31.7383 16.1744 28.9517 13.3877ZM8.875 11.25C7.56083 11.25 6.5 10.1892 6.5 8.87504C6.5 7.56087 7.56083 6.50004 8.875 6.50004C10.1892 6.50004 11.25 7.56087 11.25 8.87504C11.25 10.1892 10.1892 11.25 8.875 11.25Z"
+          />
+        </svg>
+        <p className="text-lg font-normal  text-gray-600 text-center">
+          Ultimate Discount{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+// ------------Feature Card 9 Dark--------------
+export const FeatureCard10Dark = () => {
+  return (
+    <div>
+      <div className="flex w-fit cursor-default items-center gap-4 rounded-2xl bg-gray-900 px-6 py-5">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="white"
+            d="M28.9517 13.3877L18.6125 3.04848C17.2825 1.71848 15.4458 0.958374 13.5775 0.958374H6.5C3.44416 0.958374 0.958328 3.44421 0.958328 6.50004V13.5775C0.958328 15.4775 1.70251 17.2666 3.04834 18.6124L13.3875 28.9516C14.7333 30.2974 16.5225 31.0417 18.4225 31.0417C20.3383 31.0417 22.1275 30.2974 23.4733 28.9516L28.9517 23.4733C31.7383 20.6867 31.7383 16.1744 28.9517 13.3877ZM8.875 11.25C7.56083 11.25 6.5 10.1892 6.5 8.87504C6.5 7.56087 7.56083 6.50004 8.875 6.50004C10.1892 6.50004 11.25 7.56087 11.25 8.87504C11.25 10.1892 10.1892 11.25 8.875 11.25Z"
+          />
+        </svg>
+        <p className="text-center text-lg font-normal text-gray-400">
+          Ultimate Discount
+        </p>
       </div>
     </div>
   );
