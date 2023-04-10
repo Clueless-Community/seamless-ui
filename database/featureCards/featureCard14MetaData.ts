38410a81b08fba5478330218f51ae69051cf6eb3
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-10 18:33:47 +0530

    all feature card added

---
+++ database/featureCards/featureCard14MetaData.ts
@@ -0,0 +1,43 @@
+const featureCard14MetaData = [
+{
+label: 'Light',
+content: `<div class="flex w-fit flex-row items-center gap-4 rounded-2xl bg-white px-6 py-7">
+<svg width="58" height="54" viewBox="0 0 58 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M57.1024 24.186C54.2374 38.874 39.1804 49.191 32.8414 52.947C31.6534 53.646 30.3244 54 28.9954 54C27.6664 54 26.3374 53.649 25.1494 52.947C18.8074 49.188 3.75043 38.874 0.885428 24.186C-0.311572 18.045 1.31743 11.535 5.24443 6.765C8.83843 2.403 13.8334 0 19.3114 0C22.7584 0 26.0854 0.873 28.9954 2.532C31.9054 0.873 35.2324 0 38.6794 0C44.1574 0 49.1524 2.403 52.7464 6.765C56.6734 11.535 58.3054 18.045 57.1054 24.186H57.1024Z" fill="#121225" />
+</svg>
+
+<div>
+  <p class="text-2xl font-bold leading-8 text-black">What is Web3?</p>
+  <p class="mt-2 h-12 w-96 font-normal leading-4 text-gray-600">Slate helps you see how many more days you need ve helps you how many more days</p>
+</div>
+</div>`
+},
+{
+label: 'Blue',
+content: `<div class="flex w-fit flex-row items-center gap-4 rounded-2xl bg-white px-6 py-7">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-16 w-16">
+  <path fill="#2D70F3" d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+</svg>
+
+<div>
+  <p class="text-2xl font-bold leading-8 text-blue-600">What is Web3?</p>
+  <p class="mt-2 h-12 w-96 font-normal leading-4 text-gray-600">Slate helps you see how many more days you need ve helps you how many more days</p>
+</div>
+</div>`
+},
+{
+label: 'Dark',
+content: `<div class="flex w-fit flex-row items-center gap-4 rounded-2xl bg-slate-900 px-6 py-7">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-16 w-16">
+  <path fill="white" d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+</svg>
+
+<div>
+  <p class="text-2xl font-bold leading-8 text-white">What is Web3?</p>
+  <p class="mt-2 h-12 w-96 font-normal leading-4 text-gray-400">Slate helps you see how many more days you need ve helps you how many more days</p>
+</div>
+</div>`
+},
+]
+
+export default featureCard14MetaData;
