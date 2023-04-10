8dd27194e9c12e0c50c7de4b161dd88380648fb7
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-10 22:26:36 +0530

    content card added

---
+++ database/contentCards/contentCard1MetaData.ts
@@ -0,0 +1,52 @@
+const contentCard1MetaData = [
+{
+label: 'Light',
+content: `<div class="flex w-fit flex-col items-center gap-10 rounded-2xl bg-white md:flex-row">
+<img src="https://i.ibb.co/RBm1vmK/images-5.png" alt="images-5" border="0" class="h-56" />
+<div class="flex flex-col items-start pb-8 md:pb-0 md:pr-8">
+  <p class="pb-2 text-sm font-bold tracking-wide text-gray-400">10 June 2023</p>
+  <h1 class="pb-1 text-2xl font-bold text-black">WaivlyFunds</h1>
+  <p class="w-56 text-sm font-normal leading-4 tracking-wide md:w-96">Slate helps you see how many more days you need to work to reach. Slate helps you see how many more days.</p>
+  <div class="flex gap-4 pt-4">
+    <button class="rounded-full bg-black px-3 py-0.5 text-center text-sm font-medium tracking-wide text-white">Tags</button>
+    <button class="rounded-full bg-black px-3 py-0.5 text-center text-sm font-medium tracking-wide text-white">Google</button>
+    <button class="rounded-full bg-black px-3 py-0.5 text-center text-sm font-medium tracking-wide text-white">Nvidia</button>
+  </div>
+</div>
+</div>`
+},
+{
+label: 'Blue',
+content: `<div class="flex w-fit flex-col items-center gap-10 rounded-2xl bg-white md:flex-row">
+<img src="https://i.ibb.co/RBm1vmK/images-5.png" alt="images-5" border="0" class="h-56" />
+<div class="flex flex-col items-start pb-8 md:pb-0 md:pr-8">
+  <p class="pb-2 text-sm font-bold tracking-wide text-gray-400">10 June 2023</p>
+  <h1 class="pb-1 text-2xl font-bold text-blue-600">WaivlyFunds</h1>
+  <p class="w-56 text-sm font-normal leading-4 tracking-wide md:w-96">Slate helps you see how many more days you need to work to reach. Slate helps you see how many more days.</p>
+  <div class="flex gap-4 pt-4">
+    <button class="rounded-full bg-blue-600 px-3 py-0.5 text-center text-sm font-medium tracking-wide text-white">Tags</button>
+    <button class="rounded-full bg-blue-600 px-3 py-0.5 text-center text-sm font-medium tracking-wide text-white">Google</button>
+    <button class="rounded-full bg-blue-600 px-3 py-0.5 text-center text-sm font-medium tracking-wide text-white">Nvidia</button>
+  </div>
+</div>
+</div>`
+},
+{
+label: 'Dark',
+content: `<div class="flex w-fit flex-col items-center gap-10 rounded-2xl bg-slate-900 md:flex-row">
+<img src="https://i.ibb.co/RBm1vmK/images-5.png" alt="images-5" border="0" class="h-56" />
+<div class="flex flex-col items-start pb-8 md:pb-0 md:pr-8">
+  <p class="pb-2 text-sm font-bold tracking-wide text-gray-400">10 June 2023</p>
+  <h1 class="pb-1 text-2xl font-bold text-white">WaivlyFunds</h1>
+  <p class="w-56 text-sm font-normal leading-4 tracking-wide text-gray-300 md:w-96">Slate helps you see how many more days you need to work to reach. Slate helps you see how many more days.</p>
+  <div class="flex gap-4 pt-4">
+    <button class="rounded-full bg-white px-3 py-0.5 text-center text-sm font-medium tracking-wide text-gray-900">Tags</button>
+    <button class="rounded-full bg-white px-3 py-0.5 text-center text-sm font-medium tracking-wide text-gray-900">Google</button>
+    <button class="rounded-full bg-white px-3 py-0.5 text-center text-sm font-medium tracking-wide text-gray-900">Nvidia</button>
+  </div>
+</div>
+</div>`
+},
+]
+
+export default contentCard1MetaData;
