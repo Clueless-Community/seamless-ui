d0714a1954cb3fa71e941eebcd8ac251cc18085c
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-11 06:43:56 +0530

    testimonial 3 added

---
+++ database/testimonials/testimonial2MetaData.ts
@@ -0,0 +1,166 @@
+const testimonial2MetaData = [
+{
+label: 'Light',
+content: `<div>
+
+<img src="https://i.ibb.co/N9d8Pmm/cover.png" alt="cover" class="rounded-t-2xl w-96">
+<section
+  class="section p-5 flex flex-col justify-center items-center w-96 rounded-b-2xl bg-white">
+
+  <div class="heading text-center font-semibold mb-2 mt-0">
+
+      <h1 class="text-slate-900 text-2xl font-bold">Regina Miss
+      </h2>
+      <h4 class="text-base font-bold text-gray-400">Designer</h4>
+  </div>
+
+  <div class="stars flex gap-2 mb-2">
+
+      <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+          stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+          stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+          stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 stroke-pink-600 stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 24 24" strokeWidth= {1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 stroke-pink-600 stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 24 24" strokeWidth= {1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+
+  </div>
+  <div class="text max-w-sm text-center">
+      <p class="font-sans">This proved to be impossible using consept of space and time.Einstein
+          new
+          view of time first
+          and
+          then</p>
+  </div>
+</section>
+</div>`
+},
+{
+label: 'Blue',
+content: `<div>
+<div class="img max-w-sm">
+  <a href="#">
+      <img class="w-auto rounded-t-2xl"
+          src="https://i.ibb.co/6R15rKH/cover.png" alt="cover" border="0">
+  </a>
+</div>
+
+<section
+  class="section p-5 flex flex-col justify-center items-center max-w-sm rounded-b-2xl border border-gray-100">
+
+  <div class="heading text-center font-semibold mb-2 mt-0">
+
+      <h1 class="text-blue-600 text-2xl font-bold">Regina Miss
+      </h2>
+      <h4 class="text-base">Designer</h4>
+  </div>
+
+  <div class="stars flex gap-2 mb-2">
+
+      <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+          stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+          stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+          stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 stroke-pink-600 stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 24 24" strokeWidth= {1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+      <svg class="w-4 h-4 stroke-pink-600 stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 24 24" strokeWidth= {1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+      </svg>
+
+  </div>
+  <div class="text max-w-sm text-center">
+      <p class="font-sans">This proved to be impossible using consept of space and time.Einstein
+          new
+          view of time first
+          and
+          then</p>
+  </div>
+</section>
+</div>`
+},
+{
+label: 'Dark',
+content: `<div>
+<img src="https://i.ibb.co/N9d8Pmm/cover.png" alt="cover" class="rounded-t-2xl w-96">
+<section class="section p-5 flex flex-col justify-center items-center w-96 rounded-b-2xl bg-slate-900">
+<div class="heading text-center font-semibold mb-2 mt-0">
+  <h1 class="text-white text-2xl font-bold">Regina Miss
+  <h4 class="text-base font-bold text-gray-400">Designer</h4>
+</div>
+<div class="stars flex gap-2 mb-2">
+  <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round"
+    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+  </svg>
+  <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+  stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round"
+  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+  </svg>
+  <svg class="w-4 h-4 fill-pink-600 stroke-pink-600 stroke-[3px]"
+  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth= {1.5}
+  stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round"
+  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+  </svg>
+  <svg class="w-4 h-4 stroke-pink-600 stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none"
+  viewBox="0 0 24 24" strokeWidth= {1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round"
+  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+  </svg>
+  <svg class="w-4 h-4 stroke-pink-600 stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none"
+  viewBox="0 0 24 24" strokeWidth= {1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round"
+  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+  </svg>
+</div>
+<div class="text max-w-sm text-gray-400 text-center">
+  <p class="font-sans">This proved to be impossible using consept of space and time.Einstein new view of time first and then</p>
+</div>
+</section>
+</div>`
+},
+]
+
+export default testimonial2MetaData;
