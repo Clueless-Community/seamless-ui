153b4d2c4f7498a3c6f91a59f6aec69cc65d957c
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-12 21:45:44 +0530

    6th pricing Card updated

---
+++ database/pricingCards/pricingCard5MetaData.ts
@@ -0,0 +1,67 @@
+const  pricingCard5MetaData = [
+{
+label: "Light",
+content: `<div class="w-fit rounded-2xl bg-white px-10 py-8 max-[640px]:px-3 sm:flex md:flex lg:flex">
+<div class="md:max-w-56 mt-4 md:mr-6 lg:mr-9">
+  <h1 class="text-2xl font-bold text-slate-900 max-[640px]:text-center">STANDARD</h1>
+  <p class="text-md mb-5 mt-5 font-normal text-gray-600 max-[640px]:w-72 max-[640px]:text-center sm:w-72">Slate helps you see how many more days you need to work to reach. Slate helps you how many more days.</p>
+</div>
+<div>
+  <div class="ml-1 mt-1 flex justify-center text-slate-900">
+    <p class="text-6xl font-semibold">0</p>
+    <div class="ml-2 mt-1 flex flex-col">
+      <p class="text-xl font-bold">$</p>
+      <p class="text-xl font-bold">per Month</p>
+    </div>
+  </div>
+  <div class="mt-9 flex justify-center">
+    <button class="w-fit rounded-md bg-slate-900 px-10 py-2 font-semibold text-white transition-all ease-out hover:scale-105 hover:shadow-lg">Try For Free</button>
+  </div>
+</div>
+</div>`
+},
+{
+label: "Blue",
+content: `<div class="w-fit rounded-2xl bg-white px-10 py-8 max-[640px]:px-3 sm:flex md:flex lg:flex">
+<div class="md:max-w-56 mt-4 md:mr-6 lg:mr-9">
+  <h1 class="text-2xl font-bold text-blue-600 max-[640px]:text-center">STANDARD</h1>
+  <p class="text-md mb-5 mt-5 font-normal text-gray-600 max-[640px]:w-72 max-[640px]:text-center sm:w-72">Slate helps you see how many more days you need to work to reach. Slate helps you how many more days.</p>
+</div>
+<div>
+  <div class="ml-1 mt-1 flex justify-center text-blue-600">
+    <p class="text-6xl font-semibold">0</p>
+    <div class="ml-2 mt-1 flex flex-col">
+      <p class="text-xl font-bold">$</p>
+      <p class="text-xl font-bold">per Month</p>
+    </div>
+  </div>
+  <div class="mt-9 flex justify-center">
+    <button class="w-fit rounded-md bg-blue-600 px-10 py-2 font-semibold text-white hover:shadow-lg">Try For Free</button>
+  </div>
+</div>
+</div>`
+},
+{
+label: "Dark",
+content: `<div class="w-fit rounded-2xl bg-slate-900 px-10 py-8 max-[640px]:px-3 sm:flex md:flex lg:flex">
+<div class="md:max-w-56 mt-4 md:mr-6 lg:mr-9">
+  <h1 class="text-2xl font-bold text-white max-[640px]:text-center">STANDARD</h1>
+  <p class="text-md mb-5 mt-5 font-normal text-gray-400 max-[640px]:w-72 max-[640px]:text-center sm:w-72">Slate helps you see how many more days you need to work to reach. Slate helps you how many more days.</p>
+</div>
+<div>
+  <div class="ml-1 mt-1 flex justify-center text-white">
+    <p class="text-6xl font-semibold">0</p>
+    <div class="ml-2 mt-1 flex flex-col">
+      <p class="text-xl font-bold">$</p>
+      <p class="text-xl font-bold">per Month</p>
+    </div>
+  </div>
+  <div class="mt-9 flex justify-center">
+    <button class="w-fit rounded-md bg-white px-10 py-2 font-semibold text-slate-900">Try For Free</button>
+  </div>
+</div>
+</div>`
+}
+]
+
+export default pricingCard5MetaData;
--- database/pricingCards/pricingCards.tsx
+++ database/pricingCards/pricingCards.tsx
@@ -1068,3 +1068,102 @@ export const PricingCard4Dark = () => {
     </div>
   );
 };
+
+// ------------Pricing Card 5 Light--------------
+export const PricingCard5Light = () => {
+  return (
+    <div>
+      <div className="w-fit rounded-2xl bg-white px-10 py-8 max-[640px]:px-3 sm:flex md:flex lg:flex">
+        <div className="md:max-w-56 mt-4 md:mr-6 lg:mr-9">
+          <h1 className="text-2xl font-bold text-gray-900 max-[640px]:text-center">
+            STANDARD
+          </h1>
+          <p className="text-md mb-5 mt-5 font-normal text-gray-600 max-[640px]:w-72 max-[640px]:text-center sm:w-72">
+            Slate helps you see how many more days you need to work to reach.
+            Slate helps you how many more days.
+          </p>
+        </div>
+        <div>
+          <div className="ml-1 mt-1 flex text-gray-900 justify-center text-slate-900">
+            <p className="text-6xl font-semibold">0</p>
+            <div className="ml-2 mt-1 flex flex-col">
+              <p className="text-xl font-bold">$</p>
+              <p className="text-xl font-bold">per Month</p>
+            </div>
+          </div>
+          <div className="mt-9 flex justify-center">
+            <button className="w-fit rounded-md bg-slate-900 px-10 py-2  font-semibold bg-gray-900 text-white transition-all ease-out hover:scale-105 hover:shadow-lg">
+              Try For Free
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 5 Blue--------------
+export const PricingCard5Blue = () => {
+  return (
+    <div>
+      <div className="w-fit rounded-2xl bg-white px-10 py-8 max-[640px]:px-3 sm:flex md:flex lg:flex">
+        <div className="md:max-w-56 mt-4 md:mr-6 lg:mr-9">
+          <h1 className="text-2xl font-bold text-blue-600 max-[640px]:text-center">
+            STANDARD
+          </h1>
+          <p className="text-md mb-5 mt-5 font-normal text-gray-600 max-[640px]:w-72 max-[640px]:text-center sm:w-72">
+            Slate helps you see how many more days you need to work to reach.
+            Slate helps you how many more days.
+          </p>
+        </div>
+        <div>
+          <div className="ml-1 mt-1 flex justify-center text-blue-600">
+            <p className="text-6xl font-semibold">0</p>
+            <div className="ml-2 mt-1 flex flex-col">
+              <p className="text-xl font-bold">$</p>
+              <p className="text-xl font-bold">per Month</p>
+            </div>
+          </div>
+          <div className="mt-9 flex justify-center">
+            <button className="w-fit rounded-md bg-blue-600 px-10 py-2 font-semibold text-white hover:shadow-lg">
+              Try For Free
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 5 Dark--------------
+export const PricingCard5Dark = () => {
+  return (
+    <div>
+      <div className="w-fit rounded-2xl bg-gray-900 px-10 py-8 max-[640px]:px-3 sm:flex md:flex lg:flex">
+        <div className="md:max-w-56 mt-4 md:mr-6 lg:mr-9">
+          <h1 className="text-2xl font-bold text-white max-[640px]:text-center">
+            STANDARD
+          </h1>
+          <p className="text-md mb-5 mt-5 font-normal text-gray-400 max-[640px]:w-72 max-[640px]:text-center sm:w-72">
+            Slate helps you see how many more days you need to work to reach.
+            Slate helps you how many more days.
+          </p>
+        </div>
+        <div>
+          <div className="ml-1 mt-1 flex justify-center text-white">
+            <p className="text-6xl font-semibold">0</p>
+            <div className="ml-2 mt-1 flex flex-col">
+              <p className="text-xl font-bold">$</p>
+              <p className="text-xl font-bold">per Month</p>
+            </div>
+          </div>
+          <div className="mt-9 flex justify-center">
+            <button className="w-fit rounded-md bg-white text-gray-900 px-10 py-2 font-semibold text-slate-900">
+              Try For Free
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
