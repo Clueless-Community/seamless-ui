c6b07104510e55963080a02eddfb48c5d2049f98
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-12 20:25:37 +0530

    2 pricing Cards added

---
+++ database/pricingCards/pricingCards.tsx
@@ -0,0 +1,415 @@
+import React from "react";
+
+// ------------Pricing Card 0 Light--------------
+export const PricingCard0Light = () => {
+  return (
+    <div>
+      <div className="w-64">
+        <div className="rounded-2xl bg-white px-5 py-10 shadow">
+          <p className="text-center text-2xl font-bold text-black">Free</p>
+
+          <h3 className="mt-4 text-center text-sm font-normal text-gray-800">
+            <p>Slate helps you see how many</p>
+            <p>more days you need</p>
+          </h3>
+
+          <div className="mt-5 flex justify-center text-black">
+            <div className="text-6xl font-medium">0</div>
+            <div className="ml-2 flex flex-col">
+              <p className="text-lg font-bold">$</p>
+              <p>per Month</p>
+            </div>
+          </div>
+
+          <div className="ml-3 mt-8">
+            <ul className="grid gap-4">
+              <li className="flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Unlimited updates
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Slate helps you see how
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Amazing offers
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                1 GB Storage
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Community Support
+              </li>
+            </ul>
+          </div>
+
+          <div className="mt-8">
+            <button className="w-full rounded-lg bg-black px-3 py-2 text-base font-medium text-white hover:bg-gray-800">
+              Try For Free
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 0 Blue--------------
+export const PricingCard0Blue = () => {
+  return (
+    <div>
+      <div className="w-64">
+        <div className="rounded-2xl bg-white px-5 py-10 shadow">
+          <p className="text-center text-2xl font-bold text-blue-600">Free</p>
+
+          <h3 className="mt-4 text-center text-sm font-normal text-gray-800">
+            <p>Slate helps you see how many</p>
+            <p>more days you need</p>
+          </h3>
+
+          <div className="mt-5 flex justify-center text-blue-600">
+            <div className="text-6xl font-medium">0</div>
+            <div className="ml-2 flex flex-col">
+              <p className="text-lg font-bold">$</p>
+              <p>per Month</p>
+            </div>
+          </div>
+
+          <div className="ml-3 mt-8">
+            <ul className="grid gap-4">
+              <li className="flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Unlimited updates
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Slate helps you see how
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Amazing offers
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                1 GB Storage
+              </li>
+
+              <li className="inline-flex items-center text-black">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Community Support
+              </li>
+            </ul>
+          </div>
+
+          <div className="mt-8">
+            <button className="w-full rounded-lg bg-blue-600 px-3 py-2 text-base font-medium text-white hover:bg-blue-700">
+              Try For Free
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 0 Dark--------------
+export const PricingCard0Dark = () => {
+  return (
+    <div>
+      <div className="w-64">
+        <div className="rounded-2xl bg-gray-900 px-5 py-10 shadow">
+          <p className="text-center text-2xl font-bold text-white">Free</p>
+
+          <h3 className="mt-4 text-center text-sm font-normal text-gray-400">
+            <p>Slate helps you see how many</p>
+            <p>more days you need</p>
+          </h3>
+
+          <div className="mt-5 flex justify-center text-white">
+            <div className="text-6xl font-medium">0</div>
+            <div className="ml-2 flex flex-col">
+              <p className="text-lg font-bold">$</p>
+              <p>per Month</p>
+            </div>
+          </div>
+
+          <div className="ml-3 mt-8">
+            <ul className="grid gap-4">
+              <li className="flex items-center text-white">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Unlimited updates
+              </li>
+
+              <li className="inline-flex items-center text-white">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Slate helps you see how
+              </li>
+
+              <li className="inline-flex items-center text-white">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-green-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Amazing offers
+              </li>
+
+              <li className="inline-flex items-center text-white">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                1 GB Storage
+              </li>
+
+              <li className="inline-flex items-center text-white">
+                <svg
+                  className="mr-3 h-4 w-4 fill-current text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+                </svg>
+                Community Support
+              </li>
+            </ul>
+          </div>
+
+          <div className="mt-8">
+            <button className="w-full rounded-lg bg-white px-3 py-2 text-base font-medium text-black hover:bg-gray-300">
+              Try For Free
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 1 Light--------------
+export const PricingCard1Light = () => {
+  return (
+    <div>
+      <div className="w-fit rounded-[15px] bg-white py-12 text-black">
+        <div className="flex justify-center pb-[30px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            width="64"
+            height="61"
+            className="fill-black"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <h2 className="text-center text-2xl font-bold">Standard</h2>
+        <div className="mb-4 mt-2 flex justify-center gap-4 font-bold">
+          <div className="place-self-center">
+            <p className="text-[4rem]">0</p>
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-2xl">$</p>
+            <p className="text-xl">per month</p>
+          </div>
+        </div>
+        <div className="w-72 px-[30px] text-center text-gray-900">
+          <p>
+            Slate helps you see how many more days you need to work to reach.
+            Slate helps you see how many more days.
+          </p>
+        </div>
+        <div className="mt-11 px-6">
+          <button className="w-full rounded-md bg-black px-4 py-2 text-lg font-medium text-white transition-all hover:bg-gray-800 disabled:bg-gray-400">
+            Try For Free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 1 Blue--------------
+export const PricingCard1Blue = () => {
+  return (
+    <div>
+      <div className="w-fit rounded-[15px] bg-white py-12 text-blue-600">
+        <div className="flex justify-center pb-[30px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            width="64"
+            height="61"
+            className="fill-blue-600"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <h2 className="text-center text-2xl font-bold">Standard</h2>
+        <div className="mb-4 mt-2 flex justify-center gap-4 font-bold">
+          <div className="place-self-center">
+            <p className="text-[4rem]">0</p>
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-2xl">$</p>
+            <p className="text-xl">per month</p>
+          </div>
+        </div>
+        <div className="w-72 px-[30px] text-center text-gray-900">
+          <p>
+            Slate helps you see how many more days you need to work to reach.
+            Slate helps you see how many more days.
+          </p>
+        </div>
+        <div className="mt-11 px-6">
+          <button className="w-full rounded-md bg-blue-600 px-4 py-2 text-lg font-medium text-white transition-all hover:bg-blue-700 disabled:bg-gray-400">
+            Try For Free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 1 Dark--------------
+export const PricingCard1Dark = () => {
+  return (
+    <div>
+      <div className="w-fit rounded-[15px] bg-[#121225] py-12 text-[#ffffff]">
+        <div className="flex justify-center pb-[30px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            width="64"
+            height="61"
+            className="fill-white"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <h2 className="text-center text-2xl font-bold">Standard</h2>
+        <div className="mb-4 mt-2 flex justify-center gap-4 font-bold">
+          <div className="place-self-center">
+            <p className="text-[4rem]">0</p>
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-2xl">$</p>
+            <p className="text-xl">per month</p>
+          </div>
+        </div>
+        <div className="w-72 px-[30px] text-center text-[#B5B5B5]">
+          <p>
+            Slate helps you see how many more days you need to work to reach.
+            Slate helps you see how many more days.
+          </p>
+        </div>
+        <div className="mt-11 px-6">
+          <button className="w-full rounded-md bg-white px-4 py-2 text-lg font-medium text-black transition-all hover:bg-gray-200 disabled:bg-gray-400">
+            Try For Free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
