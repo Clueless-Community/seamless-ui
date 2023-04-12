8f6b3b21ec896ba12d0ec19a477034ba88f21fe3
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-12 21:38:37 +0530

    5th pricing Card updated

---
+++ database/pricingCards/pricingCard4MetaData.ts
@@ -0,0 +1,73 @@
+const  pricingCard4MetaData = [
+{
+label: "Light",
+content: `<div class="flex w-fit">
+<div class="rounded-2xl bg-white px-10 py-10 shadow-md">
+  <div class="mt-2 flex flex-col">
+    <p class="ml-2 mt-1 w-52 text-center text-lg font-medium text-gray-600">Organize across the world</p>
+    <h1 class="mt-8 text-center text-4xl font-bold text-slate-900">FREE</h1>
+  </div>
+
+  <div>
+    <p class="mt-6 w-56 text-center text-sm text-gray-600">Slate helps you see how many days you need to work to reach.Slate helps you see how many days.</p>
+  </div>
+
+  <div class="mb-3 ml-1 mt-6 flex justify-center text-slate-900">
+    <p class="text-6xl font-semibold">0</p>
+    <div class="ml-2 mt-1 flex flex-col">
+      <p class="text-lg font-bold">$</p>
+      <p class="text-lg font-bold">per Month</p>
+    </div>
+  </div>
+</div>
+</div>`
+},
+{
+label: "Blue",
+content: `<div class="flex w-fit">
+<div class="rounded-2xl bg-white px-10 py-8 shadow-md">
+  <div class="mt-2 flex flex-col">
+    <p class="ml-2 mt-1 w-52 text-center text-lg font-medium text-gray-600">Organize across the world</p>
+    <h1 class="mt-8 text-center text-4xl font-bold text-blue-600">FREE</h1>
+  </div>
+
+  <div>
+    <p class="mt-6 w-56 text-center text-sm text-gray-600">Slate helps you see how many days you need to work to reach.Slate helps you see how many days.</p>
+  </div>
+
+  <div class="mb-3 ml-1 mt-6 flex justify-center">
+    <p class="text-6xl font-semibold text-blue-600">0</p>
+    <div class="ml-2 mt-1 flex flex-col text-blue-600">
+      <p class="text-lg font-bold">$</p>
+      <p class="text-lg font-bold">per Month</p>
+    </div>
+  </div>
+</div>
+</div>`
+},
+{
+label: "Dark",
+content: `<div class="flex w-[280px]">
+<div class="rounded-2xl bg-[#121225] px-10 py-8 shadow-md">
+  <div class="mt-2 flex flex-col">
+    <p class="ml-2 mt-1 w-52 text-center text-lg font-medium text-[#B5B5B5]">Organize across the world</p>
+    <h1 class="mt-8 text-center text-4xl font-bold text-white">FREE</h1>
+  </div>
+
+  <div>
+    <p class="mt-6 w-56 text-center text-sm text-[#B5B5B5]">Slate helps you see how many days you need to work to reach.Slate helps you see how many days.</p>
+  </div>
+
+  <div class="mb-3 ml-1 mt-6 flex justify-center">
+    <p class="text-6xl font-semibold text-white">0</p>
+    <div class="ml-2 mt-1 flex flex-col text-white">
+      <p class="text-lg font-bold">$</p>
+      <p class="text-lg font-bold">per Month</p>
+    </div>
+  </div>
+</div>
+</div>`
+}
+]
+
+export default pricingCard4MetaData;
--- database/pricingCards/pricingCards.tsx
+++ database/pricingCards/pricingCards.tsx
@@ -963,3 +963,108 @@ export const PricingCard3Dark = () => {
     </div>
   );
 };
+
+// ------------Pricing Card 4 Light--------------
+export const PricingCard4Light = () => {
+  return (
+    <div>
+      <div className="flex w-fit">
+        <div className="rounded-2xl bg-white px-10 py-10 shadow-md">
+          <div className="mt-2 flex flex-col">
+            <p className="ml-2 mt-1 w-52 text-center text-lg font-medium text-gray-600">
+              Organize across the world
+            </p>
+            <h1 className="mt-8 text-center text-4xl text-black font-bold text-slate-900">
+              FREE
+            </h1>
+          </div>
+
+          <div>
+            <p className="mt-6 w-56 text-center text-sm text-gray-600">
+              Slate helps you see how many days you need to work to reach.Slate
+              helps you see how many days.
+            </p>
+          </div>
+
+          <div className="mb-3 ml-1 mt-6 text-black flex justify-center text-slate-900">
+            <p className="text-6xl font-semibold">0</p>
+            <div className="ml-2 mt-1 flex flex-col">
+              <p className="text-lg font-bold">$</p>
+              <p className="text-lg font-bold">per Month</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 4 Blue--------------
+export const PricingCard4Blue = () => {
+  return (
+    <div>
+      <div className="flex w-fit">
+        <div className="rounded-2xl bg-white px-10 py-8 shadow-md">
+          <div className="mt-2 flex flex-col">
+            <p className="ml-2 mt-1 w-52 text-center text-lg font-medium text-gray-600">
+              Organize across the world
+            </p>
+            <h1 className="mt-8 text-center text-4xl font-bold text-blue-600">
+              FREE
+            </h1>
+          </div>
+
+          <div>
+            <p className="mt-6 w-56 text-center text-sm text-gray-600">
+              Slate helps you see how many days you need to work to reach.Slate
+              helps you see how many days.
+            </p>
+          </div>
+
+          <div className="mb-3 ml-1 mt-6 flex justify-center">
+            <p className="text-6xl font-semibold text-blue-600">0</p>
+            <div className="ml-2 mt-1 flex flex-col text-blue-600">
+              <p className="text-lg font-bold">$</p>
+              <p className="text-lg font-bold">per Month</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 4 Dark--------------
+export const PricingCard4Dark = () => {
+  return (
+    <div>
+      <div className="flex w-[280px]">
+        <div className="rounded-2xl bg-[#121225] px-10 py-8 shadow-md">
+          <div className="mt-2 flex flex-col">
+            <p className="ml-2 mt-1 w-52 text-center text-lg font-medium text-[#B5B5B5]">
+              Organize across the world
+            </p>
+            <h1 className="mt-8 text-center text-4xl font-bold text-white">
+              FREE
+            </h1>
+          </div>
+
+          <div>
+            <p className="mt-6 w-56 text-center text-sm text-[#B5B5B5]">
+              Slate helps you see how many days you need to work to reach.Slate
+              helps you see how many days.
+            </p>
+          </div>
+
+          <div className="mb-3 ml-1 mt-6 flex justify-center">
+            <p className="text-6xl font-semibold text-white">0</p>
+            <div className="ml-2 mt-1 flex flex-col text-white">
+              <p className="text-lg font-bold">$</p>
+              <p className="text-lg font-bold">per Month</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
