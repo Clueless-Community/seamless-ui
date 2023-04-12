1f2c68e71141a8fe29f2b4198227695b9b66fa9d
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-12 21:17:01 +0530

    3rd pricing Card updated

--- database/pricingCards/pricingCards.tsx
+++ database/pricingCards/pricingCards.tsx
@@ -413,3 +413,478 @@ export const PricingCard1Dark = () => {
     </div>
   );
 };
+
+// ------------Pricing Card 2 Light--------------
+export const PricingCard2Light = () => {
+  return (
+    <div>
+      <div className="relative w-80 rounded-[20px] bg-white px-[40px] py-[50px]">
+        <div className="flex w-full flex-col items-center justify-center gap-[15px] tracking-[0.2px]">
+          <h1 className="text-[24px] font-bold leading-[32px] text-black">
+            Free
+          </h1>
+          <h2 className="text-center font-sans text-lg text-black">
+            Slate helps you see how many more days you need
+          </h2>
+          <div className="my-0 flex justify-between px-5 font-semibold text-black">
+            <div className="my-1 text-[64px]">19</div>
+            <div className="ml-3 flex flex-col">
+              <div className="mt-3 text-[24px] font-bold">$</div>
+              <div className="text-[20px]">per month</div>
+            </div>
+          </div>
+          <button className="mb-1 w-full rounded-[5px] bg-black px-[40px] py-[15px] font-sans text-[18px] font-semibold text-white hover:bg-gray-800">
+            Try for Free
+          </button>
+          <div className="flex w-full flex-col items-center justify-center gap-4 pt-4">
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Unlimited product updates
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Slate helps you see how
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Amazing offers
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  className="mr-2 mt-1 h-6 w-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 32 32"
+                >
+                  <rect width="32" height="32" fill="#BDBDBD" rx="16" />
+                  <g clip-path="url(#a)">
+                    <path
+                      fill="#fff"
+                      fill-rule="evenodd"
+                      d="M24.296 10.204a.698.698 0 0 1 0 .985l-9.738 9.738a.694.694 0 0 1-.985 0l-4.869-4.869a.697.697 0 0 1 .985-.985l4.376 4.378 9.246-9.247a.698.698 0 0 1 .985 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path
+                        fill="#fff"
+                        d="M0 0h16v11H0z"
+                        transform="translate(8 10)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                1GB Cloud Storage
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  className="mr-2 mt-1 h-6 w-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 32 32"
+                >
+                  <rect width="32" height="32" fill="#BDBDBD" rx="16" />
+                  <g clip-path="url(#a)">
+                    <path
+                      fill="#fff"
+                      fill-rule="evenodd"
+                      d="M24.296 10.204a.698.698 0 0 1 0 .985l-9.738 9.738a.694.694 0 0 1-.985 0l-4.869-4.869a.697.697 0 0 1 .985-.985l4.376 4.378 9.246-9.247a.698.698 0 0 1 .985 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path
+                        fill="#fff"
+                        d="M0 0h16v11H0z"
+                        transform="translate(8 10)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Email and community support
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 2 Blue--------------
+export const PricingCard2Blue = () => {
+  return (
+    <div>
+      <div className="relative w-80 rounded-[20px] bg-white px-[40px] py-[50px]">
+        <div className="flex w-full flex-col items-center justify-center gap-[15px] tracking-[0.2px]">
+          <h1 className="text-[24px] font-bold leading-[32px] text-blue-600">
+            Free
+          </h1>
+          <h2 className="text-center font-sans text-lg text-black">
+            Slate helps you see how many more days you need
+          </h2>
+          <div className="my-0 flex justify-between px-5 font-semibold text-blue-600">
+            <div className="my-1 text-[64px]">19</div>
+            <div className="ml-3 flex flex-col">
+              <div className="mt-3 text-[24px] font-bold">$</div>
+              <div className="text-[20px]">per month</div>
+            </div>
+          </div>
+          <button className="mb-1 w-full rounded-[5px] bg-blue-600 px-[40px] py-[15px] font-sans text-lg font-semibold text-white hover:bg-blue-700">
+            Try for Free
+          </button>
+          <div className="flex w-full flex-col items-center justify-center gap-4 pt-4">
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Unlimited product updates
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Slate helps you see how
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Amazing offers
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  className="mr-2 mt-1 h-6 w-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 32 32"
+                >
+                  <rect width="32" height="32" fill="#BDBDBD" rx="16" />
+                  <g clip-path="url(#a)">
+                    <path
+                      fill="#fff"
+                      fill-rule="evenodd"
+                      d="M24.296 10.204a.698.698 0 0 1 0 .985l-9.738 9.738a.694.694 0 0 1-.985 0l-4.869-4.869a.697.697 0 0 1 .985-.985l4.376 4.378 9.246-9.247a.698.698 0 0 1 .985 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path
+                        fill="#fff"
+                        d="M0 0h16v11H0z"
+                        transform="translate(8 10)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                1GB Cloud Storage
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  className="mr-2 mt-1 h-6 w-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 32 32"
+                >
+                  <rect width="32" height="32" fill="#BDBDBD" rx="16" />
+                  <g clip-path="url(#a)">
+                    <path
+                      fill="#fff"
+                      fill-rule="evenodd"
+                      d="M24.296 10.204a.698.698 0 0 1 0 .985l-9.738 9.738a.694.694 0 0 1-.985 0l-4.869-4.869a.697.697 0 0 1 .985-.985l4.376 4.378 9.246-9.247a.698.698 0 0 1 .985 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path
+                        fill="#fff"
+                        d="M0 0h16v11H0z"
+                        transform="translate(8 10)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-black">
+                Email and community support
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 2 Dark--------------
+export const PricingCard2Dark = () => {
+  return (
+    <div>
+      <div className="relative w-80 rounded-[20px] bg-black px-[40px] py-[50px]">
+        <div className="flex w-full flex-col items-center justify-center gap-[15px] tracking-[0.2px]">
+          <h1 className="text-[24px] font-bold leading-[32px] text-white">
+            Free
+          </h1>
+
+          <h2 className="text-center font-sans text-lg text-[#B5B5B5]">
+            Slate helps you see how many more days you need
+          </h2>
+
+          <div className="my-0 flex justify-between px-5 font-semibold text-[#FFFFFF]">
+            <div className="my-1 text-[64px]">19</div>
+            <div className="ml-3 flex flex-col">
+              <div className="mt-3 text-[24px] font-bold">$</div>
+              <div className="text-[20px]">per month</div>
+            </div>
+          </div>
+
+          <button className="mb-1 w-full rounded-[5px] bg-[#FEFEFE] px-[40px] py-[15px] font-sans text-lg font-semibold text-[#121225] hover:bg-gray-300">
+            Try for Free
+          </button>
+
+          <div className="flex w-full flex-col items-center justify-center gap-4 pt-4">
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-[#B5B5B5]">
+                Unlimited product updates
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-[#B5B5B5]">
+                Slate helps you see how
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#3dbb77"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-[#B5B5B5]">
+                Amazing offers
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  className="mr-2 mt-1 h-6 w-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 32 32"
+                >
+                  <rect width="32" height="32" fill="#BDBDBD" rx="16" />
+                  <g clip-path="url(#a)">
+                    <path
+                      fill="#fff"
+                      fill-rule="evenodd"
+                      d="M24.296 10.204a.698.698 0 0 1 0 .985l-9.738 9.738a.694.694 0 0 1-.985 0l-4.869-4.869a.697.697 0 0 1 .985-.985l4.376 4.378 9.246-9.247a.698.698 0 0 1 .985 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path
+                        fill="#fff"
+                        d="M0 0h16v11H0z"
+                        transform="translate(8 10)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-[#B5B5B5]">
+                1GB Cloud Storage
+              </p>
+            </div>
+
+            <div className="flex w-full items-center justify-start gap-[8px]">
+              <div className="h-[32px] w-[32px]">
+                <svg
+                  className="mr-2 mt-1 h-6 w-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 32 32"
+                >
+                  <rect width="32" height="32" fill="#BDBDBD" rx="16" />
+                  <g clip-path="url(#a)">
+                    <path
+                      fill="#fff"
+                      fill-rule="evenodd"
+                      d="M24.296 10.204a.698.698 0 0 1 0 .985l-9.738 9.738a.694.694 0 0 1-.985 0l-4.869-4.869a.697.697 0 0 1 .985-.985l4.376 4.378 9.246-9.247a.698.698 0 0 1 .985 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path
+                        fill="#fff"
+                        d="M0 0h16v11H0z"
+                        transform="translate(8 10)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+
+              <p className="text-[14px] font-medium text-[#B5B5B5]">
+                Email and community support
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
