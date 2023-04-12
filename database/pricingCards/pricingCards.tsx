4faa3ff028f05e0195ac34d0b88e3188613efa75
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-12 21:29:39 +0530

    4th pricing Card updated

--- database/pricingCards/pricingCards.tsx
+++ database/pricingCards/pricingCards.tsx
@@ -888,3 +888,78 @@ export const PricingCard2Dark = () => {
     </div>
   );
 };
+
+// ------------Pricing Card 3 Light--------------
+export const PricingCard3Light = () => {
+  return (
+    <div>
+      <div className="flex h-[355px] w-[280px] flex-col gap-y-5 rounded-2xl bg-white p-8">
+        <h2 className="text-3xl font-bold text-black">FREE</h2>
+        <p className="leading-5 text-black">
+          Slate helps you see how many more days you need to work to reach.
+          Slate helps you see how many more days.
+        </p>
+        <div className="flex items-center justify-start">
+          <h3 className="text-7xl font-semibold text-black">0</h3>
+          <div className="ml-4">
+            <h4 className="text-2xl font-[1000] text-black">$</h4>
+            <h4 className="text-xl font-semibold text-black">per month</h4>
+          </div>
+        </div>
+        <button className="rounded bg-black py-3 font-semibold text-white">
+          Try for Free
+        </button>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 3 Blue--------------
+export const PricingCard3Blue = () => {
+  return (
+    <div>
+      <div className="flex h-[355px] w-[280px] flex-col gap-y-5 rounded-2xl bg-white p-8">
+        <h2 className="text-3xl font-bold text-blue-600">FREE</h2>
+        <p className="leading-5 text-black">
+          Slate helps you see how many more days you need to work to reach.
+          Slate helps you see how many more days.
+        </p>
+        <div className="flex items-center justify-start">
+          <h3 className="text-7xl font-semibold text-blue-600">0</h3>
+          <div className="ml-4">
+            <h4 className="text-2xl font-[1000] text-blue-600">$</h4>
+            <h4 className="text-xl font-semibold text-blue-600">per month</h4>
+          </div>
+        </div>
+        <button className="rounded bg-blue-600 py-3 font-semibold text-white">
+          Try for Free
+        </button>
+      </div>
+    </div>
+  );
+};
+
+// ------------Pricing Card 3 Dark--------------
+export const PricingCard3Dark = () => {
+  return (
+    <div>
+      <div className="flex h-[355px] w-[280px] flex-col gap-y-5 rounded-2xl bg-[#121225] p-8">
+        <h2 className="text-3xl font-bold text-white">FREE</h2>
+        <p className="leading-5 text-[#B5B5B5]">
+          Slate helps you see how many more days you need to work to reach.
+          Slate helps you see how many more days.
+        </p>
+        <div className="flex items-center justify-start">
+          <h3 className="text-7xl font-semibold text-white">0</h3>
+          <div className="ml-4">
+            <h4 className="text-2xl font-[1000] text-white">$</h4>
+            <h4 className="text-xl font-semibold text-white">per month</h4>
+          </div>
+        </div>
+        <button className="rounded bg-[#F5F1F1] py-3 font-semibold text-black">
+          Try for Free
+        </button>
+      </div>
+    </div>
+  );
+};
