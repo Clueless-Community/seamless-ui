75591b1de2fd102d1b8725da373a341d2946633a
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-13 18:46:51 +0530

    files added in form

---
+++ database/forms/emailRoundedMetaData.ts
@@ -0,0 +1,30 @@
+const emailRoundedMetaData = [
+{
+label: "Dark",
+content: `<div class="grid gap-5 p-5">
+<!-- small -->
+<input type="email" class="w-full rounded-full bg-slate-900 p-2 pl-5 text-sm text-normal text-white placeholder-gray-400 outline-none focus:border-blue-600 disabled:border-gray-300 disabled:bg-gray-300" placeholder="Email *" />
+
+<!-- medium -->
+<input type="email" class="text-md w-full rounded-full bg-slate-900 p-3 pl-6 text-white placeholder-gray-400 outline-none focus:border-blue-600 disabled:border-gray-300 disabled:bg-gray-300" placeholder="Email *" />
+
+<!-- large -->
+<input type="email" class="w-full rounded-full bg-slate-900 p-5 pl-8 text-lg text-white placeholder-gray-400 outline-none focus:border-blue-600 disabled:border-gray-300 disabled:bg-gray-300" placeholder="Email *" />
+</div>`
+},
+{
+label: "Light",
+content: `<div class="grid gap-5 p-5">
+<!-- small -->
+<input type="email" class="w-full rounded-full border-white bg-white p-2 pl-5 text-sm text-normal text-black placeholder-[#737373]" placeholder="Email *" />
+
+<!-- medium -->
+<input type="email" class="text-md w-full rounded-full border-white bg-white p-3 pl-6 text-black placeholder-[#737373]" placeholder="Email *" />
+
+<!-- large -->
+<input type="email" class="w-full rounded-full border-white bg-white p-5 pl-8 text-lg text-black placeholder-[#737373]" placeholder="Email *" />
+</div>`
+}
+]
+
+export default emailRoundedMetaData;
